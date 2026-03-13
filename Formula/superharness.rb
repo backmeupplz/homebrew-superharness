ef465babaa7bca41f28ca893bb45d674f0e58e80
Nikita Kolmogorov <git@bdut.ch>
2026-03-13 09:28:20 -0700

    init: create main branch for homebrew tap

---
+++ Formula/superharness.rb
@@ -0,0 +1,7 @@
+# Formula will be auto-updated by CI on each release
+class Superharness < Formula
+  desc "Autonomous multi-agent orchestrator for AI coding agents via tmux"
+  homepage "https://github.com/backmeupplz/superharness"
+  version "0.0.0"
+  license "MIT"
+end
